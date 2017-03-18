97c7656c61d5c5c11668218e2bee50addf2b209b
Manel Rodero <manel@fib.upc.edu>
2017-03-18 12:07:29 +0100

    Start working in configuration scripts

---
+++ Module-2/Configure-Start.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+:: Wrapper para descargar y ejecutar el script de configuración de la VM Win81 IE11
---
+++ Module-2/Configure-Win81-IE11.ps1
@@ -0,0 +1,5 @@
+# Script de Configuración de Windows 8.1 IE11
+
+# Actualización de la ayuda de PowerShell
+Write-Host "Actualización de la ayuda de PowerShell" -Foreground Green
+Update-Help
