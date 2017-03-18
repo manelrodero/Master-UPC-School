26231d590b377390263cbd53b0602251c8bb3de6
Manel Rodero <manel@fib.upc.edu>
2017-03-18 13:05:41 +0100

    Download Git Portable

--- Module-2/Configure-Win81-IE11.ps1
+++ Module-2/Configure-Win81-IE11.ps1
@@ -6,7 +6,6 @@ Update-Help
 
 # Cmdlets para internacionalización
 # https://technet.microsoft.com/en-us/library/hh852115.aspx
-
 # Get-Command -Module International
 # [System.Globalization.CultureInfo]::GetCultures('InstalledWin32Cultures')
 
@@ -19,3 +18,20 @@ Write-Host "Deshabilitando Auto Update (Microsoft Update)" -ForegroundColor Gree
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update' -Name 'AUOptions' -Value 2
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update' -Name 'CachedAUOptions' -Value 2
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update' -Name 'IncludeRecommendedUpdates' -Value 1
+
+# Obtención de la ubicación de carpetas especiales
+# http://windowsitpro.com/powershell/easily-finding-special-paths-powershell-scripts
+# [Environment+SpecialFolder]::GetNames([Environment+SpecialFolder])
+
+# Creación de la carpeta MASTER en el escritorio
+Write-Host "Creando carpeta MASTER" -ForegroundColor Green
+$DesktopFolder = "$([Environment]::GetFolderPath("Desktop"))\MASTER"
+if (!(Test-Path -Path "$DesktopFolder")) { mkdir "$DesktopFolder" }
+
+# Tres métodos distintos para descargar ficheros en PowerShell
+# https://blog.jourdant.me/post/3-ways-to-download-files-with-powershell
+# Descarga Git-Portable
+Write-Host "Descargando Git-Portable... " -ForegroundColor Green -NoNewline
+$start_time = Get-Date
+Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.12.0.windows.1/PortableGit-2.12.0-64-bit.7z.exe -OutFile "$DesktopFolder\PortableGit-2.12.0-32-bit.7z.exe"
+Write-Host "$((Get-Date).Subtract($start_time).Seconds) segundo(s)"
