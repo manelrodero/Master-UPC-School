0d8151d6095031dd6a4855a4d8f0d3fb26cba729
Manel Rodero <manel@fib.upc.edu>
2017-03-18 12:36:10 +0100

    Cambio de teclado a es-ES

--- Module-2/Configure-Win81-IE11.ps1
+++ Module-2/Configure-Win81-IE11.ps1
@@ -2,4 +2,14 @@
 
 # Actualización de la ayuda de PowerShell
 Write-Host "Actualizando ayuda de PowerShell" -ForegroundColor Green
-Update-Help -Force
+Update-Help
+
+# Cmdlets para internacionalización
+# https://technet.microsoft.com/en-us/library/hh852115.aspx
+
+# Get-Command -Module International
+# [System.Globalization.CultureInfo]::GetCultures('InstalledWin32Cultures')
+
+# Cambio del teclado en-US a es-ES ;-)
+Write-Host "Cambiando teclado a es-ES" -ForegroundColor Green
+Set-WinUserLanguageList -LanguageList es-ES -Force
