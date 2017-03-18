e7388c5441766c6bdcfa16009aa9e37cab3a41e4
Manel Rodero <manel@fib.upc.edu>
2017-03-18 12:13:51 +0100

    Use PowerShell.exe to download PS1 script

--- Module-2/Configure-Start.bat
+++ Module-2/Configure-Start.bat
@@ -1,3 +1,5 @@
 @ECHO OFF
 
 :: Wrapper para descargar y ejecutar el script de configuración de la VM Win81 IE11
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/manelrodero/Master-UPC-School/start-edits/Module-2/Configure-Win81-IE11.ps1')"
