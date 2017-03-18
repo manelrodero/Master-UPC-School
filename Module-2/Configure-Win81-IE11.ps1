ee57a0a45480fe2a0ad666aa938a74900e2056ee
Manel Rodero <manel@fib.upc.edu>
2017-03-18 12:30:00 +0100

    Force help update

--- Module-2/Configure-Win81-IE11.ps1
+++ Module-2/Configure-Win81-IE11.ps1
@@ -1,5 +1,5 @@
 # Script de Configuración de Windows 8.1 IE11
 
 # Actualización de la ayuda de PowerShell
-Write-Host "Actualización de la ayuda de PowerShell" -Foreground Green
-Update-Help
+Write-Host "Actualizando ayuda de PowerShell" -ForegroundColor Green
+Update-Help -Force
