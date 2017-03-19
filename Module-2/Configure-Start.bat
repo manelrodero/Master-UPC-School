45134d38ae94d025488f45ca39e6aedc1af7155b
Manel Rodero <manel@fib.upc.edu>
2017-03-19 22:48:07 +0100

    Cambio URL script Start-Configure.ps1

--- Module-2/Configure-Start.bat
+++ Module-2/Configure-Start.bat
@@ -2,4 +2,4 @@
 
 :: Wrapper para descargar y ejecutar el script de configuración de la VM Win81 IE11
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/manelrodero/Master-UPC-School/start-edits/Module-2/Configure-Win81-IE11.ps1')"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://github.com/manelrodero/Master-UPC-School/raw/master/Module-2/Configure-Win81-IE11.ps1')"
