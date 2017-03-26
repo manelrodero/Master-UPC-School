f2ba5250634e575858121716e75543fa05bb7a86
Manel Rodero <manel@fib.upc.edu>
2017-03-26 18:47:52 +0200

    Descarga e instalación de NirCmd

--- Module-2/Configure-Win81-IE11.ps1
+++ Module-2/Configure-Win81-IE11.ps1
@@ -40,11 +40,11 @@ if (!(Test-Path -Path "$DesktopFolder\Downloads")) { mkdir "$DesktopFolder\Downl
 # https://blog.jourdant.me/post/3-ways-to-download-files-with-powershell
 
 # Descarga 7-Zip
-$prog7zip = "7z1604.msi"
-if (!(Test-Path -Path "$DesktopFolder\Downloads\$prog7zip")) {
+$progDownload = "7z1604.msi"
+if (!(Test-Path -Path "$DesktopFolder\Downloads\$progDownload")) {
     Write-Host "Descargando 7-Zip ... " -ForegroundColor Green -NoNewline
     $start_time = Get-Date
-    Invoke-WebRequest http://www.7-zip.org/a/$prog7zip -OutFile "$DesktopFolder\Downloads\$prog7zip"
+    Invoke-WebRequest http://www.7-zip.org/a/$progDownload -OutFile "$DesktopFolder\Downloads\$progDownload"
     Write-Host "$((Get-Date).Subtract($start_time).Seconds) segundo(s)" -ForegroundColor Yellow
 } else {
     Write-Host "7-Zip ya estaba descargado" -ForegroundColor Yellow
@@ -52,15 +52,15 @@ if (!(Test-Path -Path "$DesktopFolder\Downloads\$prog7zip")) {
 
 # Instalación de 7-Zip
 Write-Host "Instalando 7-Zip ... " -ForegroundColor Green -NoNewline
-msiexec.exe /i "$DesktopFolder\Downloads\$prog7zip" /passive
+msiexec.exe /i "$DesktopFolder\Downloads\$progDownload" /passive
 Write-Host "OK" -ForegroundColor Yellow
 
 # Descarga Git-Portable
-$progGit = "PortableGit-2.12.1-32-bit.7z.exe"
-if (!(Test-Path -Path "$DesktopFolder\Downloads\$progGit")) {
+$progDownload = "PortableGit-2.12.1-32-bit.7z.exe"
+if (!(Test-Path -Path "$DesktopFolder\Downloads\$progDownload")) {
     Write-Host "Descargando Git-Portable ... " -ForegroundColor Green -NoNewline
     $start_time = Get-Date
-    Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.12.1.windows.1/$progGit -OutFile "$DesktopFolder\Downloads\$progGit"
+    Invoke-WebRequest https://github.com/git-for-windows/git/releases/download/v2.12.1.windows.1/$progDownload -OutFile "$DesktopFolder\Downloads\$progDownload"
     Write-Host "$((Get-Date).Subtract($start_time).Seconds) segundo(s)" -ForegroundColor Yellow
 } else {
     Write-Host "Git-Portable ya esta descargado" -ForegroundColor Yellow
@@ -69,15 +69,15 @@ if (!(Test-Path -Path "$DesktopFolder\Downloads\$progGit")) {
 # Instalación de Git-Portable
 Write-Host "Descomprimiendo Git Portable ... " -ForegroundColor Green -NoNewline
 if (Test-Path -Path "$DesktopFolder\Git") { Remove-Item -Path "$DesktopFolder\Git" -Recurse -Force }
-& "$env:ProgramFiles\7-Zip\7z.exe" x -o"$DesktopFolder\Git" -y "$DesktopFolder\Downloads\$progGit" | Out-Null
+& "$env:ProgramFiles\7-Zip\7z.exe" x -o"$DesktopFolder\Git" -y "$DesktopFolder\Downloads\$progDownload" | Out-Null
 Write-Host "OK" -ForegroundColor Yellow
 
 # Descarga Sysinternals Suite
-$progSysinternals = "SysinternalsSuite.zip"
-if (!(Test-Path -Path "$DesktopFolder\Downloads\$progSysinternals")) {
+$progDownload = "SysinternalsSuite.zip"
+if (!(Test-Path -Path "$DesktopFolder\Downloads\$progDownload")) {
     Write-Host "Descargando Sysinternals Suite ... " -ForegroundColor Green -NoNewline
     $start_time = Get-Date
-    Invoke-WebRequest https://download.sysinternals.com/files/$progSysinternals -OutFile "$DesktopFolder\Downloads\$progSysinternals"
+    Invoke-WebRequest https://download.sysinternals.com/files/$progDownload -OutFile "$DesktopFolder\Downloads\$progDownload"
     Write-Host "$((Get-Date).Subtract($start_time).Seconds) segundo(s)" -ForegroundColor Yellow
 } else {
     Write-Host "SysinternalsSuite ya esta descargado" -ForegroundColor Yellow
@@ -86,12 +86,32 @@ if (!(Test-Path -Path "$DesktopFolder\Downloads\$progSysinternals")) {
 # Instalación de Sysinternals Suite
 Write-Host "Descomprimiendo Sysinternals Suite ... " -ForegroundColor Green -NoNewline
 if (Test-Path -Path "$DesktopFolder\Sysinternals") { Remove-Item -Path "$DesktopFolder\Sysinternals" -Recurse -Force }
-& "$env:ProgramFiles\7-Zip\7z.exe" x -o"$DesktopFolder\Sysinternals" -y "$DesktopFolder\Downloads\$progSysinternals" | Out-Null
+& "$env:ProgramFiles\7-Zip\7z.exe" x -o"$DesktopFolder\Sysinternals" -y "$DesktopFolder\Downloads\$progDownload" | Out-Null
+Write-Host "OK" -ForegroundColor Yellow
+
+# Descarga NirCmd
+$progDownload = "nircmd.zip"
+if (!(Test-Path -Path "$DesktopFolder\Downloads\$progDownload")) {
+    Write-Host "Descargando NirCmd ... " -ForegroundColor Green -NoNewline
+    $start_time = Get-Date
+    Invoke-WebRequest http://www.nirsoft.net/utils/$progDownload -OutFile "$DesktopFolder\Downloads\$progDownload"
+    Write-Host "$((Get-Date).Subtract($start_time).Seconds) segundo(s)" -ForegroundColor Yellow
+} else {
+    Write-Host "SysinternalsSuite ya esta descargado" -ForegroundColor Yellow
+}
+
+# Instalación de NirCmd
+Write-Host "Descomprimiendo NirCmd ... " -ForegroundColor Green -NoNewline
+if (Test-Path -Path "$DesktopFolder\Nirsoft") { Remove-Item -Path "$DesktopFolder\Nirsoft" -Recurse -Force }
+& "$env:ProgramFiles\7-Zip\7z.exe" x -o"$DesktopFolder\Nirsoft" -y "$DesktopFolder\Downloads\$progDownload" | Out-Null
 Write-Host "OK" -ForegroundColor Yellow
 
 # Descarga scripts del Máster
 if (!(Test-Path -Path "$DesktopFolder\Scripts")) { mkdir "$DesktopFolder\Scripts" | Out-Null }
 & "$DesktopFolder\Git\bin\git.exe" clone https://github.com/manelrodero/Master-UPC-School.git "$DesktopFolder\Scripts"
+
+# Creación carpeta C:\TEST
+if (!(Test-Path -Path "C:\TEST")) { mkdir "C:\TEST" | Out-Null }
 
 # Final del script (evitar que se cierre)
 Write-Host "Pulsa una tecla para continuar ..."
