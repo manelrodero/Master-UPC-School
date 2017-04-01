c018bd1ff74415fa789582cf175e0df536a1c968
Manel Rodero <manel@fib.upc.edu>
2017-04-01 23:16:52 +0200

    Instalación de NXLog-CE y fichero de configuración

---
+++ Module-2/test-nxlog.bat
@@ -0,0 +1,19 @@
+@echo off
+
+net session >nul 2>&1
+if not "%errorLevel%" == "0" (
+    echo Es necesario ejecutar el script como Administrador!
+    pause
+    exit /b %errorLevel%
+)
+
+echo Instalando NXLog-CE...
+msiexec /passive /i "C:\Users\IEUser\Desktop\MASTER\Downloads\nxlog-ce-2.9.1716.msi"
+
+echo Copiando script nxlog.conf...
+copy /y "C:\Users\IEUser\Desktop\MASTER\Scripts\Module-2\nxlog.conf" "%ProgramFiles%\nxlog\conf\nxlog.master.conf" >nul 2>&1
+
+echo Agregar la IP del Syslog remoto en "%ProgramFiles%\nxlog\conf\nxlog.conf" usando protocolo UDP
+echo Reiniciar el servicio 'nxlog'
+pause
+start "" notepad.exe "%ProgramFiles%\nxlog\conf\nxlog.conf"
