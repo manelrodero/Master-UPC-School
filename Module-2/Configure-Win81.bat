804cffe76a651d1d6796575e61c0f0fd02b525ce
Manel Rodero <manel@fib.upc.edu>
2018-03-27 22:35:52 +0200

    Forzar TLS1.2 para descargar desde GitHub

--- Module-2/Configure-Win81.bat
+++ Module-2/Configure-Win81.bat
@@ -2,4 +2,12 @@
 
 :: Wrapper para descargar y ejecutar el script de configuración de la VM Win81 IE11
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-Expression (New-Object Net.WebClient).DownloadString('https://github.com/manelrodero/Master-UPC-School/raw/master/Module-2/Configure-Win81-IE11.ps1')"
+copy nul %~n0.ps1 > nul
+
+:: GitHub elimina TLS1.0 y TLS1.1 (https://githubengineering.com/crypto-removal-notice/)
+>> %~n0.ps1 echo [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+>> %~n0.ps1 echo Invoke-Expression (New-Object Net.WebClient).DownloadString('https://github.com/manelrodero/Master-UPC-School/raw/master/Module-2/Configure-Win81-IE11.ps1')
+
+PowerShell -NoProfile -ExecutionPolicy Bypass .\%~n0.ps1
+
+rem del %~n0.ps1
