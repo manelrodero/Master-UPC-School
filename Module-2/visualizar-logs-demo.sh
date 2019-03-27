8acbb28607147fdd36064c5ef0e640b9b0107f59
unknown <Manel@LEVIATHAN.local>
2019-03-27 14:47:34 +0100

    Cambio de logs de Ubuntu a Lubuntu

--- Module-2/visualizar-logs-demo.sh
+++ Module-2/visualizar-logs-demo.sh
@@ -10,43 +10,43 @@ echo "  cat /var/log/auth.log"
 read -p "Presione una tecla para continuar . . ."&&clear
 
 # Mostrar las líneas que coinciden con un patrón (siguientes 9 con -A, anteriores con -B, cercanas con -C)
-grep "Initializing HighMem" /var/log/dmesg
+grep "session opened for user" /var/log/auth.log
 echo ""
 echo "- Búsqueda de un patrón mediante el comando 'grep'"
-echo "  grep \"Initializing HighMem\" /var/log/dmesg"
+echo "  grep \"session opened for user\" /var/log/auth.log"
 read -p "Presione una tecla para continuar . . ."&&clear
 
 # Mostrar las primeras 10 líneas de un fichero
-head -n 10 /var/log/boot.log
+head -n 10 /var/log/bootstrap.log
 echo ""
 echo "- Muestra las primeras 10 líneas de un fichero mediante el comando 'head'"
-echo "  head -n 10 /var/log/boot.log"
+echo "  head -n 10 /var/log/bootstrap.log"
 read -p "Presione una tecla para continuar . . ."&&clear
 
 # Mostrar las últimas 10 líneas de un fichero
-tail -n 10 /var/log/boot.log
+tail -n 10 /var/log/bootstrap.log
 echo ""
 echo "- Muestra las últimas 10 líneas de un fichero mediante el comando 'tail'"
-echo "  tail -n 10 /var/log/boot.log"
+echo "  tail -n 10 /var/log/bootstrap.log"
 read -p "Presione una tecla para continuar . . ."&&clear
 
 # Ignorar las primeras 10 líneas de un fichero
-tail -n +11 /var/log/boot.log
+tail -n +11 /var/log/bootstrap.log
 echo ""
 echo "- Ignora las primeras 10 líneas de un fichero mediante el comando 'tail'"
-echo "  tail -n +11 /var/log/boot.log"
+echo "  tail -n +11 /var/log/bootstrap.log"
 read -p "Presione una tecla para continuar . . ."&&clear
 
 # Buscar un patrón (y líneas cercanas) en un fichero comprimido
-zcat /var/log/dmesg.1.gz | grep -C 2 "error"
+zcat /var/log/apt/history.log.1.gz | grep -C 2 "Commandline:"
 echo ""
 echo "- Busca un patrón en un fichero comprimido mediante el comando 'zcat'"
-echo "  zcat /var/log/dmesg.1.gz | grep -C 2 \"error\""
+echo "  zcat /var/log/apt/history.log.1.gz | grep -C 2 \"Commandline:\""
 read -p "Presione una tecla para continuar . . ."&&clear
 
 # Visualiza un fichero numerando las líneas
-cat -n /var/log/boot.log
+cat -n /var/log/bootstrap.log
 echo ""
 echo "- Visualiza un fichero numerando las líneas"
-echo "  cat -n /var/log/boot.log"
+echo "  cat -n /var/log/bootstrap.log"
 read -p "Presione una tecla para continuar . . ."&&clear
