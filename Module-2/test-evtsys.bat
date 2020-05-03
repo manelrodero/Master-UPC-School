e6b0b6b935e486398e94354053ccb675cb1e29e0
Manel Rodero <manelrodero@users.noreply.github.com>
2020-05-03 19:52:41 +0200

    Configuración IP 192.168.56.16 (Syslog)

--- Module-2/test-evtsys.bat
+++ Module-2/test-evtsys.bat
@@ -1,11 +1,9 @@
 @echo off
 
-if [%1]==[] goto :eof
-
 echo Test Event to Syslog
 
 cd "%userprofile%\Desktop\MASTER\EventSyslog"
-echo Instalando evtsys -i -a -h %1 -f daemon -l 3 -p 514...
-evtsys -i -a -h %1 -f daemon -l 3 -p 514
+echo Instalando evtsys -i -a -h 192.168.56.16 -f daemon -l 3 -p 514...
+evtsys -i -a -h 192.168.56.16 -f daemon -l 3 -p 514
 net start evtsys
-eventcreate /t warning /id 1000 /l Application /D "Prueba"
+eventcreate /t warning /id 1000 /l Application /D "Prueba desde Windows (evtsys)"
--- Module-2/test-nxlog.bat
+++ Module-2/test-nxlog.bat
@@ -15,11 +15,6 @@ echo Copiando script nxlog.conf...
 copy /y "%ProgramFiles%\nxlog\conf\nxlog.conf" "%ProgramFiles%\nxlog\conf\nxlog.original.conf" >nul 2>&1
 copy /y "C:\Users\IEUser\Desktop\MASTER\Scripts\Module-2\nxlog.conf" "%ProgramFiles%\nxlog\conf\nxlog.conf" >nul 2>&1
 
-echo.
-echo A continuación se lanzará Notepad++ para editar el fichero "%ProgramFiles%\nxlog\conf\nxlog.conf":
-echo 1 - Indicar la IP del Syslog remoto usando protocolo UDP
-echo 2 - Reiniciar el servicio 'nxlog':
-echo     * net stop nxlog
-echo     * net start nxlog
-pause
-start "" "..\..\Notepad++\notepad++.exe" "%ProgramFiles%\nxlog\conf\nxlog.conf"
+echo Reiniciando NXLog-CE...
+net stop nxlog
+net start nxlog
